e5dec2594ae2e1527e9a521702d66616a9b5412a
Gian Luiz <102696502+gianluiz@users.noreply.github.com>
2022-10-25 16:15:28 -0300

    Update adicionando números inteiros na Lista.py

--- adicionando números inteiros na Lista.py
+++ adicionando números inteiros na Lista.py
@@ -1,5 +1,6 @@
 lista1 = list()
-while True:
+pergunta = ""
+while pergunta in "S":
     while True:
         try:
             n = int(input("Digite o VALOR:"))
@@ -10,16 +11,13 @@ while True:
         lista1.append(n)
     elif n in lista1:
         print("Esse número já está na lista...\nDIGITE OUTRO.")
-    pergunta = " "
-    while pergunta != "SN":
-        pergunta = str(input('Quer continuar?\n[S] para SIM.\n[N] para NÃO.\n->')).strip().upper()
+    while True:
+        pergunta = input('Quer continuar?\n[S] para SIM.\n[N] para NÃO.\n->').strip().upper()
         if len(pergunta) == 0:
-            print("ERRO.\nDigite uma resposta válida!\n[S] para SIM ou [N] para NÃO.")
+            print("ERRO.\nDigite [S] para SIM e [N] para NÃO.")
         elif pergunta in "SN":
             break
         else:
-            print("ERRO.\nDigite uma resposta válida!\n[S] para SIM ou [N] para NÃO.")
-    if pergunta in 'N':
-        break
+            print("Resposta inválida.")
 lista1.sort()
 print(lista1)
